1c40fde19fb9b924593968415a3bc336a50700a2
filipj169 <jamrichfilip16@gmail.com>
2025-07-07 14:05:53 +0200

    server initialization

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"log"
 )
 
 func main() {
-	fmt.Println("Start...")
+	router := http.NewServeMux()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Received request for '/'")
+	})
+
+	server := http.Server{
+		Addr:   ":8080",
+		Handler: router,
+	}
+
+	log.Println("Starting server on :8080")
+	server.ListenAndServe()
 }
---
+++ models/Book.go
@@ -0,0 +1,10 @@
+package models
+
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   int    `json:"year"`
+}
+
+
