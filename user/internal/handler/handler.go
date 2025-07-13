d8188273135e7676e1ef5c953df81ef5adaef89d
filipj169 <jamrichfilip16@gmail.com>
2025-07-13 16:48:27 +0200

    basic register handler

--- user/internal/handler/handler.go
+++ user/internal/handler/handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/focuscw0w/microservices/user/internal/service"
@@ -16,8 +18,24 @@ func (h *Handler) HandleHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
 	var req service.CreateUserRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
 
-	h.UserService.Create(&req)
+	err = h.UserService.Create(&req)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("create user failed: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("User registered successfully"))
 }
